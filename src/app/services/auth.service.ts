24120739960c824157409a68abf8bcbbaef522a0
Smit Sonani <smitsonani7777@gmail.com>
2025-10-23 19:56:44 +0530

    API Url fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,7 +21,7 @@ export interface AuthResponse {
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = `${environment.apiUrl}`;
+  private apiUrl = `${environment.apiUrl}/api`;
 
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
