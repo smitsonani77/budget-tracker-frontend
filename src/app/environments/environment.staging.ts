7db643e3f9f2a85f4d73d41ccad7d09d32a42829
Smit Sonani <smitsonani7777@gmail.com>
2025-10-23 19:32:23 +0530

    build issue

---
+++ src/app/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://budget-tracker-backend-hazel.vercel.app',
+  appName: 'Budget Tracker',
+  version: '1.0.0',
+  enableDebug: true,
+};
